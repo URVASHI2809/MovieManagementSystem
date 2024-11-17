de49c47a20790a106f0a407d236cc314e9becdb8
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2024-11-18 01:02:18 +0530

    Update MovieDatabase.java

--- MovieDatabase.java
+++ MovieDatabase.java
@@ -6,23 +6,7 @@ public class MovieDatabase {
     public void addMovie(Movie movie) {
         if (movieMap.containsKey(movie.getId())) {
             System.out.println("Movie with ID already exists.");
-        } else {
-            movieMap.put(movie.getId(), movie);
-            System.out.println("Movie '" + movie.getTitle() + "' added successfully");
-        }
-    }
-
-    public List<Movie> searchMovies(String title, String genre, int year, double minRating) {
-        System.out.println("Searching in Primary Store");
-        List<Movie> result = new ArrayList<>();
-        for (Movie movie : movieMap.values()) {
-            if ((title == null || movie.getTitle().equalsIgnoreCase(title)) &&
-                    (genre == null || movie.getGenre().equalsIgnoreCase(genre)) &&
-                    (year <= 0 || movie.getReleaseYear() == year) &&
-                    (minRating <= 0.0 || movie.getRating() >= minRating)) {
-                result.add(movie);
-            }
-        }
+        
         return result;
     }
 }
