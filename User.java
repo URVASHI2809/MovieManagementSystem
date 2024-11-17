4fdaec45336f05dcc0e742e2b72f2f4c61f788a8
Urvashi Prajapati <76160571+URVASHI2809@users.noreply.github.com>
2024-11-18 01:02:48 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,20 +1,12 @@
 public class User {
-    private int id;
-    private String name;
-    private String preferredGenre;
-
-    public User(int id, String name, String preferredGenre) {
-        this.id = id;
-        this.name = name;
-        this.preferredGenre = preferredGenre;
+rredGenre;
     }
 
     public int getId() { return id; }
     public String getName() { return name; }
-    public String getPreferredGenre() { return preferredGenre; }
-
+    pub
     @Override
     public String toString() {
-        return String.format("User{id=%d, name='%s', preferredGenre='%s'}", id, name, preferredGenre);
+        return String.format("User{id=%d, name='%s', preferredGenre='%redGenre);
     }
 }
